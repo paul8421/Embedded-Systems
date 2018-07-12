688531d43b228bb32e33f2bb29534bb6ca74e990
paul <s8dipaul@stud.uni-saarland.de>
2018-07-12 18:53:46 +0200

    RF Interrupt working

--- Project_Template/Project_Template/src/collector/main.cpp
+++ Project_Template/Project_Template/src/collector/main.cpp
@@ -1,55 +1,72 @@
-#include <Arduino.h>
-#include <Zumo32U4.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <3pi.h>
+#include <stdlib.h>
 #include <nrf24.h>
 #include <nRF24L01.h>
-#include "../../libs/arduino/HardwareSerial.h"
-#include "math.h"
+#include <math.h>
+//#include "../../../../../../../Arduino/hardware/tools/avr/avr/include/time.h"
+#include <stdio.h>
 
 /******************************RF***********************************/
-int flag = 1;
-char buff[2] = {'O', 'K'};
-char buff1[3] = {'N', 'O', 'K'};
+char buff5[2] = {'2', '3'};
+char no_ack[3] = {'A', 'C', 'K'};
 char buff3[1] = {10};
+char rcv_data[4];
+char *v;
+int rf_int = 0;
 char reg_data[8] = {'0','0','0','0','0','0','0','0'};
-
-uint8_t tx_address[5] = {0xe1,0xf0,0xf0,0xf0,0xf0};
-uint8_t rx_address[5] = {0x12,0x12,0x66,0x4b,0x6f};
-//0x6f4b661212
-uint8_t data_array[20];
-uint8_t temp;
-uint8_t q = 0;
-/******************************RF***********************************/
-#define MODE 2
-#define  INIT_PING 2
-int m = 0;
-void dance();
-uint16_t _2hex2dec(uint8_t reg1, uint8_t reg2);
-float drive_homing(float x1, float y1, float theta, float x2, float y2);
-Zumo32U4Motors motors;
-typedef struct coordinates{
-    int x;
-    int y;
-
-}coordiantes;
-void drive();
-float distances [] = {0,0,0};
+int flag = 1;
 float find_angle(float x1, float y1, float x2, float y2);
 float find_dist(float x1, float y1, float x2, float y2);
 void turn_around(float angle);
 float drive(float x1, float y1, float x2, float y2);
-void insertionSort(float distances[], int n);
-#define  mot_spd 210
+void dance();
+float drive_homing(float x1, float y1, float theta, float x2, float y2);
+#define  mot_spd 73
 
-void  send_serial_RF(uint8_t* data_array);
 
-void setup() {
-    int m =0;
-    Serial1.begin(9600);
+uint8_t tx_address[5] = {0xe1,0xf0,0xf0,0xf0,0xf0};
+uint8_t rx_address[5] = {0x12,0x12,0x66,0x4b,0x6f};
+uint8_t data_array[32];
+
+uint8_t send_reg[32];
+uint8_t rcv_reg[32];
+
+
+char * data_rcvd;
+uint8_t temp;
+uint8_t q = 0;
+char to_serial[32];
+/******************************RF***********************************/
+void transfer_rcv(char *reg_data);
+void setBaudRate(unsigned long baud)
+
+#define  MODE 2
+/*  1 - RELAY
+ *  2 - OTHERS
+ */
+#define INIT_PING 2
+{
+#if _SERIAL_PORTS > 1  // Orangutan X2 and SVP users
+    serial_set_baud_rate(UART0, baud);
+#else
+    serial_set_baud_rate(baud);
+#endif
+}
+
+// this code sets up timer1 for a 1s  @ 16Mhz Clock (mode 4)
+void send_serial_RF(uint8_t *);
+char* rcv_serial_RF();
+int main(void) {
     nrf24_init();
-    nrf24_config(111, 10); // on channel 111 with payload length 4
+    nrf24_config(111, 15); // on channel 11 with payload length 15
     nrf24_tx_address(tx_address);
     nrf24_rx_address(rx_address);
-    Serial1.println("COLLECTOR");
+    /* send_reg[3] = 0x02; */
+    send_reg[2] = 0xff;
+    send_reg[1] = 0x31;
+    send_reg[0] = 0x50;
     DDRD &= ~(1 << DDD2);     // Clear the PD2 pin
     // PD2 (PCINT0 pin) is now an input
 
@@ -58,114 +75,124 @@ void setup() {
 
 
 
-    EICRA |= (1 << ISC10);    // set INT0 to trigger on falling edge
+    EICRA |= (1 << ISC01);    // set INT0 to trigger on falling edge
+    EICRA &= ~(1 << ISC00);
     EIMSK |= (1 << INT0);     // Turns on INT0
+    int i = 0;
+    int bytes;
+    setBaudRate(9600);
+    int hi, lo;
+    float angle3 = 0;
+    serial_send_blocking("hi\n", sizeof("hi\n"));
+    sei();
+    if (MODE == 1) {
+        serial_send_blocking("expecting serial input\n", sizeof("expecting serial input\n"));
+        while (1) {
+            bytes = 0;
+            while (bytes == 0) {
 
-    sei();                    // turn on interrupts
-}
+                while (serial_receive_blocking(buff5, sizeof(buff5), 6000000));
+                bytes = serial_get_received_bytes();
 
+            }
+            *data_array = (buff5[0] - '0') * 10 + (buff5[1] - '0');
+            //serial_send_blocking("Received the foll on serial :", sizeof("Received the foll on serial :"));
+            serial_send_blocking(buff5, sizeof(buff5));
+            serial_send_blocking("\n", sizeof("\n"));
 
+            send_serial_RF(data_array);
+            if(nrf24_dataReady())
+            {
+                serial_send_blocking("received ping back from collector\n", sizeof("received ping back from collector\n"));
+            }
 
-void loop() {
-    nrf24_tx_address(tx_address);
-    nrf24_rx_address(rx_address);
-    uint8_t send_reg[32];
-    send_reg[2] = 0xde;
-    //send_reg[3] = 0x02;
-    send_reg[1] = 0xa0;
-    send_reg[0] = 0x50;
+        }
+        //serial_send_blocking(buff5, sizeof(buff5));
+        //break;
 
-    float angle3;
-    uint8_t status = nrf24_getStatus();
-    delay(20);
-    if (nrf24_dataReady()) {
-        Serial1.println("Get data");
-        nrf24_getData(data_array);
-        delay(200);
-        switch (data_array[0]) {
-            case 0x51:
-                Serial1.println("ACK for PING\n");
-                Serial1.print(data_array[0], HEX);
-                Serial1.print(data_array[1], HEX);
-                Serial1.println(data_array[2], HEX);
-                flag = 1;
-                break;
-            case 0x50:
-                Serial1.print(data_array[0], HEX);
-                Serial1.print(data_array[1], HEX);
-                Serial1.println(data_array[2], HEX);
+    }
+    else {
+        //float angle3;
+        // enable interrupts
+        while (1) {
+            bytes = 0;
+            if(INIT_PING == 1) {
+                send_serial_RF(send_reg);
+                serial_send_blocking("Sent ping\n", sizeof("Sent ping\n"));
+            }
+            //delay(50);
+            if (rf_int == 1) {
+                rf_int = 0;
+                serial_send_blocking("Expecting data\n", sizeof("Expecting data\n"));
 
-                if (data_array[2] == 0xff)
-                    data_array[1]++;
-                data_array[0]++;
-                data_array[2]++;
-                data_array[0]= 0x15;
-                data_array[1]= 0x15;
-                data_array[2]= 0x15;
-                send_serial_RF(data_array);
-                Serial1.print(data_array[0], HEX);
-                Serial1.print(data_array[1], HEX);
-                Serial1.println(data_array[2], HEX);
-                flag = 1;
-                break;
+                switch (rcv_reg[0]) {
+                    case 0x50:
+                        rcv_reg[0]++;
+                        if (rcv_reg[2] == 0xff)
+                            rcv_reg[1]++;
+                        rcv_reg[2]++;
+                        serial_send_blocking("xxx\n", sizeof("xxx\n"));
+                        send_serial_RF(rcv_reg);
+                        break;
+                    case 0x51:
+                        lo = *rcv_reg & 0x0f;
+                        hi = (*rcv_reg >> 4) & 0x0f;
+                        sprintf(&to_serial[0], "%x", hi);
+                        sprintf(&to_serial[1], "%x", lo);
+                        lo = *(rcv_reg + 1) & 0x0f;
+                        hi = (*(rcv_reg + 1) >> 4) & 0x0f;
+                        sprintf(&to_serial[2], "%x", hi);
+                        sprintf(&to_serial[3], "%x", lo);
+                        lo = *(rcv_reg + 2) & 0x0f;
+                        hi = (*(rcv_reg + 2) >> 4) & 0x0f;
+                        sprintf(&to_serial[4], "%x", hi);
+                        sprintf(&to_serial[5], "%x", lo);
 
-            case 0x60:
-                if(flag)
-                {Serial1.println("In 0x60");
-                    motors.setSpeeds(45, 45);
-                    delay(500);
-                    flag = 0;
-                    nrf24_csn_digitalWrite(LOW);
-                    spi_transfer(FLUSH_RX);
-                    nrf24_csn_digitalWrite(HIGH);}
-                else{}
-                break;
-            case 0x61:
+                        serial_send_blocking("rcv pong\n", sizeof("rcv pong\n"));
+                        serial_send_blocking(to_serial, sizeof(to_serial));
+                        break;
+                        /*  case 0x60:
+                              if (!flag)
+                                  break;
+                              angle3 = angle3+1;
+                              angle3 = angle3-1;
 
-                Serial1.println("OOB");
-                motors.setSpeeds(0,0);
-                for(int i = 0; i<15; i++)
-                {
-                    delay(2000);
+                              angle3 = drive_homing(rcv_reg[2], rcv_reg[3], rcv_reg[1], 0, 0);
+                              dance();
+                              flag = 0;
+                              break;
+                              */
+
                 }
-
-                uint16_t angle_decode = _2hex2dec(data_array[1], data_array[2]);
-
-                float temppp = (float) angle_decode;
-
-                temppp = temppp*360;
-                temppp = temppp/65535;
-
-                delay(500);
-                int temp11 = _2hex2dec(data_array[3], data_array[4]);
-
-                int temp2 = _2hex2dec(data_array[5], data_array[6]);//rcv_reg[5]*256 + rcv_reg[6];
-                angle3 = drive_homing((float) temp11/100, (float) temp2/100, temppp, 0, 0);
-                dance();
-
-
-                while(1);
-                break;              ;
+                serial_send_blocking("\n", sizeof("\n"));
+            }
         }
     }
 }
-
-
-void  send_serial_RF(uint8_t * data_array)
+char* rcv_serial_RF(){
+    if(nrf24_dataReady())
+    {
+        nrf24_getData(rcv_reg);
+        return  data_rcvd;
+    }
+}
+void  send_serial_RF(uint8_t* data_array)
 {
+    /* Automatically goes to TX mode */
     nrf24_send(data_array);
 
+    /* Wait for transmission to end */
     while (nrf24_isSending());
+    /* Make analysis on last tranmission attempt */
     temp = nrf24_lastMessageStatus();
 
     if (temp == NRF24_TRANSMISSON_OK)
     {
-        Serial1.println("TX OK");
-
+        serial_send_blocking("trans_ok\n", sizeof("trans_ok\n"));
     }
     else if (temp == NRF24_MESSAGE_LOST) {
         {
-            Serial1.println("TX NNNNOK");
+            serial_send_blocking("msg_lost\n", sizeof("msg_lost\n"));
         }
     }
 
@@ -185,11 +212,11 @@ float find_angle(float x1, float y1, float x2, float y2)
     theta = atan(tan_theta);
     if (x_diff >= 0 && y_diff < 0 )  // 4th quad
     {
-        theta = 2*(3.14285714) + theta;
+        theta = 2*(22/7) + theta;
     }
     else if ((x_diff < 0 && y_diff >=0) || (x_diff < 0 && y_diff <0)  ) // 2nd quad and 3rd quad
     {
-        theta = theta + (3.14285714);
+        theta = theta + (22/7);
     }
 
     theta = theta*(180/3.14285714);
@@ -209,125 +236,77 @@ float find_dist(float x1, float y1, float x2, float y2)
 void turn_around(float angle)
 {
     float angle1 = 360-angle;
-    delay(1000);
-    motors.setSpeeds(-mot_spd,mot_spd);
-    delay(angle1*4.1);
-    motors.setSpeeds(0,0);
+    delay(3000);
+    set_motors(-80,80);
+    delay(angle1*2.08);
+    set_motors(0,0);
 }
 
-float drive(float x1, float y1, float x2, float y2) {
+float drive(float x1, float y1, float x2, float y2)
+{
     float angle = find_angle(x1, y1, x2, y2);
     float dist = find_dist(x1, y1, x2, y2);
-    int dist1 = dist * 400;
-    int time_motors = angle * 4.1;
+    int dist1 = dist*500;
+    int time_motors = angle * 2.22;
 
-    delay(1000);
-    motors.setSpeeds(-mot_spd, mot_spd);
+    delay(3000);
+    set_motors(-82,82);
     delay(time_motors);
-    motors.setSpeeds(0, 0);
+    set_motors(0,0);
 
-
-    motors.setSpeeds(mot_spd, mot_spd);
-    delay(dist1);
-    motors.setSpeeds(0, 0);
-
+    delay(50);
+    set_motors(mot_spd-2,mot_spd);
+    delay((dist1)*0.5);
+    set_motors((0.6*mot_spd-2), 0.6*mot_spd);
+    delay(dist1*0.2);
+    set_motors((0.3*mot_spd-2), 0.3*mot_spd);
+    delay(dist1*0.3);
+    set_motors(0,0);
     return angle;
 
 }
 
-
-float drive_homing(float x1, float y1, float theta, float x2, float y2) {
-    Serial1.println(("In drive\n"));
-    Serial1.println(x1);
-    Serial1.println(y1  );
-    Serial1.println(theta  );
-
-
-
-    if(theta >=0 && theta <= 90)
-    {
-        theta = 90 - theta;
-    }
-    else
-    {
-        theta = abs(450- theta);
-    }
+float drive_homing(float x1, float y1, float theta, float x2, float y2)
+{
     float angle = find_angle(x1, y1, x2, y2) - theta;
-    Serial1.print("find angle yields  ");
-    Serial1.println(angle+theta);
-    if(angle < 0)
-    {
-        angle = angle +360;
-    }
-
-    Serial1.print("Final angle is  :");
-    Serial1.println(angle);
     float dist = find_dist(x1, y1, x2, y2);
-    int dist1 = dist * 400;
-    int time_motors = angle * 4.1;
+    int dist1 = dist*500;
+    int time_motors = angle * 2.22;
 
-    delay(1000);
-    motors.setSpeeds(-mot_spd, mot_spd);
+    delay(3000);
+    set_motors(-82,82);
     delay(time_motors);
-    motors.setSpeeds(0, 0);
+    set_motors(0,0);
 
-
-    motors.setSpeeds(mot_spd, mot_spd);
-    delay(dist1);
-    motors.setSpeeds(0, 0);
-
+    delay(50);
+    set_motors(mot_spd-2,mot_spd);
+    delay((dist1)*0.5);
+    set_motors((0.6*mot_spd-2), 0.6*mot_spd);
+    delay(dist1*0.2);
+    set_motors((0.3*mot_spd-2), 0.3*mot_spd);
+    delay(dist1*0.3);
+    set_motors(0,0);
     return angle;
 
 }
-
 
 void dance()
 {
     int i = 0;
     while(i<5)
     {
-        motors.setSpeeds(mot_spd, -mot_spd);
+        set_motors(mot_spd, -mot_spd);
         delay(100);
-        motors.setSpeeds(-mot_spd, mot_spd);
+        set_motors(-mot_spd, mot_spd);
         delay(100);
         i++;
     }
-    motors.setSpeeds(0, 0);
+    set_motors(0, 0);
 }
 
-uint16_t _2hex2dec(uint8_t reg1, uint8_t reg2)
-{
-    char buff[50];
-    uint16_t tempalisa =  (reg1 & 0xf0);
-
-    tempalisa = (tempalisa >> 4);
-    tempalisa = tempalisa * 4096;
-
-
-    uint16_t tempalisa1 = (reg1 & 0x0f);
-    tempalisa1 = tempalisa1*256;
-
-
-    uint16_t tempalisa2 = (reg2 & 0xf0);
-    tempalisa2 = tempalisa2>>4;
-    tempalisa2 = tempalisa2 * 16;
-
-    uint16_t tempalisa3 = (reg2 & 0x0f);
-
-    uint16_t final = tempalisa+tempalisa1+tempalisa2+tempalisa3;
-
-
-
-    return final;
-}
-
-void drive()
-{
-    motors.setSpeeds(45, 45);
-}
 ISR (INT0_vect)
-{
-    play_frequency(6000, 150, 15);
-    
-
-}
+        {
+                play_frequency(6000, 150, 15);
+        nrf24_getData(rcv_reg);
+        rf_int = 1;
+        }
